4c76fcfc5cdfbf6939b19d89b8eea1987773de86
jnasante <josephnasante@yahoo.com>
2016-12-08 18:16:52 -0600

    Removed old comments and prepared for final submission

--- data_processor.py
+++ data_processor.py
@@ -69,32 +69,9 @@ def initialize():
 	print('Reading data...')
 	data = pd.read_csv('data/kddcup_normalized_50K.csv', header=None)
 
-	# Disgusting, hardcoded
-	# classification_id = {
-	# 	'normal' : 0,
-	# 	'pod' : 1,
-	# 	'neptune' : 2,
-	# 	'guess_passwd' : 3,
-	# 	'loadmodule' : 4,
-	# 	'teardrop' : 5,
-	# 	'buffer_overflow' : 6,
-	# 	'perl' : 7,
-	# 	'smurf' : 8
-	# }
-
-	# classification = reverse_map = dict(reversed(item) for item in classification_id.items())
-
-	# for i in range(1, 4):
-	# 	keys = list(set(data[i]))
-	# 	print(keys)
-		# for j in range(len(data[i])):
-		# 	print(i, j)
-		# 	data[i][j] = keys.index(data[i][j])
-
 	# X and y for our learning (as matrices)
 	X = data.as_matrix(columns=data.columns[:-1])
 	y = data[data.columns[-1]].values
-	print(list(set(y)))
 
 	num_samples = len(X)
 	num_features = len(X[0])
